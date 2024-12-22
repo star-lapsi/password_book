6d5a28d191e1c0292eb5cdc2b0182d513e88d595
star-lapsi <hypno_disc3@126.com>
2024-12-22 16:34:58 +0800

    Using functions to doing some jobs.

--- main_forms.c
+++ main_forms.c
@@ -2,20 +2,17 @@
 #include<string.h>
 struct pass_info {
     char username[50];
-    int long_un;
     char origin_pw[200];
-    int long_o_pw;
-    char new_pw[200];
-    int long_n_pw;
+    char domains[200];
     char prompt[200];
-    int long_pt;
     char telephone[20];
 };
+void data_write(struct pass_info *mem, FILE *wr);  //写入文件函数
+void data_show(struct pass_info *mem, int count);   //展示数据
 int main()
 {
     struct pass_info users[20];
-    int count=0,num=0;
-    int i,j,k;
+    int i,k,num;
     char useless_char;
     FILE *fp;
     fp=fopen("pssv.txt","w+");
@@ -23,27 +20,51 @@ int main()
     scanf("%d%c",&num,&useless_char);
     for(i=0;i<num;i++)
     {
-        printf("Please enter the username:\n");
+        printf("Now is the %d data\n",i+1);
+        printf("Please enter the username:");
         gets(users[i].username);
-        users[i].long_un=strlen(users[i].username);
-        printf("Please enter the password:\n");
+        printf("Please enter the password:");
         gets(users[i].origin_pw);
-        users[i].long_o_pw=strlen(users[i].origin_pw);
+        printf("Please enter the domain:");
+        gets(users[i].domains);
+        printf("Please enter the prompt:");
+        gets(users[i].prompt);
+        printf("Please enter the telephone:");
+        gets(users[i].telephone);
     }
-    for(j=0;j<num;j++)
-    {
-        printf("The username is:");
-        puts(users[j].username);
-        printf("The password is:");
-        puts(users[j].origin_pw);
-    }
-    for(k=0;k<num;k++)
-    {
-        fprintf(fp,"%s",users[k].username);
-        putc('\n',fp);
-        fprintf(fp,"%s",users[k].origin_pw);
-        putc('\n',fp);
-    }
+    data_show(users,num);
+    for(k=0;k<num;k++)  data_write(users+k,fp);
     fclose(fp);
     return 0;
+}
+void data_show(struct pass_info *mem, int count)
+{
+    int flag;
+    for(flag=0;flag<count;flag++,mem++)
+    {
+        printf("The username is: ");
+        puts(mem->username);
+        printf("The password is: ");
+        puts(mem->origin_pw);
+        printf("The domain is: ");
+        puts(mem->domains);
+        printf("The prompt is: ");
+        puts(mem->prompt);
+        printf("The telephone is: ");
+        puts(mem->telephone);
+    }
+}
+void data_write(struct pass_info *mem, FILE *wr)
+{
+    fprintf(wr,"%s",mem->username);
+    putc('\n',wr);
+    fprintf(wr,"%s",mem->origin_pw);
+    putc('\n',wr);
+    fprintf(wr,"%s",mem->domains);
+    putc('\n',wr);
+    fprintf(wr,"%s",mem->prompt);
+    putc('\n',wr);
+    fprintf(wr,"%s",mem->telephone);
+    putc('\n',wr);
+    return;
 }
