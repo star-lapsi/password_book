0f603d599ab8e3fc072b11cc639d5d05c81cfefd
star-lapsi <hypno_disc3@126.com>
2024-12-23 18:08:09 +0800

    input function testing

--- main_forms.c
+++ main_forms.c
@@ -1,26 +1,59 @@
 #include<stdio.h>
 #include<string.h>
 struct pass_info {
+    int xuhao;
     char username[50];
     char origin_pw[40];
     char domains[20];
     char prompt[20];
     char telephone[15];
 };
-void data_add(struct pass_info *men, int count);  //手动添加数据
+int data_read_from_file(struct pass_info *mem);    //导入外部数据
+void data_add(struct pass_info *mem, int count);  //手动添加数据
 void data_show(struct pass_info *mem, int count);   //展示当前数据
 void data_save_to_file(struct pass_info *mem, int count);  //写入文件函数
 int main()
 {
-    struct pass_info users[20];
+    struct pass_info users[50]={ 0 };
     int num;
     char useless_char;
+    num=data_read_from_file(users);
     printf("How many users do you want to add?\n");
     scanf("%d%c",&num,&useless_char);
     data_add(users,num);
     data_show(users,num);
     data_save_to_file(users,num);
     return 0;
+}
+int data_read_from_file(struct pass_info *mem)
+{
+    int tag;
+    int inc=0,ouc=0,stc=0;
+    FILE *rd;
+    rd=fopen("pssq.txt","r");
+    while((tag = getc(rd)) != EOF)
+    {
+        if(tag==';') 
+        {
+            if(ouc==4) 
+            {
+                ouc=0;
+                mem->xuhao=stc;
+                stc++;
+                mem=mem+stc;
+            }
+            else ouc++;
+            inc=0;
+
+        }
+        else if(ouc==0) mem->username[inc++]=tag;
+        else if(ouc==1) mem->origin_pw[inc++]=tag;
+        else if(ouc==2) mem->domains[inc++]=tag;
+        else if(ouc==3) mem->prompt[inc++]=tag;
+        else if(ouc==4) mem->telephone[inc++]=tag;
+    }
+    fclose(rd);
+    return stc;
 }
 void data_add(struct pass_info *mem, int count)
 {
@@ -68,15 +101,15 @@ void data_save_to_file(struct pass_info *mem, int count)
     for(flag=0;flag<count;flag++,mem++)
     {
         fprintf(wr,"%s",mem->username);
-        putc('\n',wr);
+        putc(';',wr);
         fprintf(wr,"%s",mem->origin_pw);
-        putc('\n',wr);
+        putc(';',wr);
         fprintf(wr,"%s",mem->domains);
-        putc('\n',wr);
+        putc(';',wr);
         fprintf(wr,"%s",mem->prompt);
-        putc('\n',wr);
+        putc(';',wr);
         fprintf(wr,"%s",mem->telephone);
-        putc('\n',wr);
+        putc(';',wr);
     }
     fclose(wr);
     return;
